87130d8030a5d513c41d075604e7a3488ee9dffe
Artem <adamenko.artem@gmail.com>
2014-06-26 11:17:21 +0200

    finish

---
+++ protected/modules/Admin/views/default/index.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $this DefaultController */
+
+$this->breadcrumbs=array(
+	$this->module->id,
+);
+?>
+<h1>Добро пожаловать в панель администрирования</h1>
+
---
+++ protected/views/site/final.php
@@ -0,0 +1,3 @@
+<?php
+print_r(Yii::app()->session);
+?>
